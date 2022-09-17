2c4ed60ea2d3413e2381a04f4764a49b8bf012eb
kzo <87224332+kzeo@users.noreply.github.com>
2022-09-17 12:49:36 +0200

    fix: buttons
    Implemented backend for buttons and  buttons with css

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './state'
-import CodeCell from './components/code-cell'
+import CellList from './components/cell-list'
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <CodeCell />
+        <CellList />
       </div>
     </Provider>
   )
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducers from './reducers'
+import { ActionType } from './action-types/index'
 
 export const store = configureStore({
   reducer: reducers,
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+})
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
 })
